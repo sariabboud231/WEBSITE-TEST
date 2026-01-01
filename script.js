b27325c1e00b7407f013cc52dde987fe9edc5cbd
sariabboud231 <sariabboud231@gmail.com>
2026-01-01 20:39:17 +0200

    Update and rename test.js to script.js

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const btn = document.getElementById("actionBtn");
 
 btn.addEventListener("click", () => {
   alert("You clicked the button!");
-
-  // Replace this with ANY link you want
   window.location.href = "https://buy.stripe.com/7sY3cx5Oy7JP1wSblUfbq02";
 });
